353ac7b59d4d1a339a65ec33066cb69d0d0ed3d8
With-chomok <183892208+With-chomok@users.noreply.github.com>
2024-12-03 22:36:13 +0600

    1-9 problem solve uploads

---
+++ basic1.js
@@ -0,0 +1,61 @@
+
+// problem solve.1:Create a variable called carName, assign the value Volvo to it.
+
+let carName = "Volvo" ; 
+
+console.log(carName);
+
+
+// problem solve 2:On one single line, declare three variables with the following names and values:
+
+let firstName = "john";
+let lastName = "Doe";
+let age = "35";
+
+// problem solve 3:Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+
+x = 10 ;
+y = 5 ;
+
+x = x * y;
+console.log(x);
+
+
+// problem solve 4:Use comments to describe the correct data type of the following variables:o
+
+let length = 16 ; data type : number
+let lastName = "Johnson";
+                            //  (data type : number)
+const x = {
+    firstName: "john",
+    lastName: "doe"
+};
+//  (data type : object)
+
+
+console.log(typeof x)
+
+problem solve 5:Execute the function named myFunction. or see html file
+
+function myFunction(){
+    alert("Hello World!")
+}
+myFunction();
+
+// problem 6 & 7 :- html file 
+
+// problem solve 8: Array Related Question(html file check please)
+
+
+    // solve 1: Alert the number of items in an array, using the correct Array property: 
+
+
+    const car =  ["volvo" , "jeep" , "Mercedes"];
+    console.log(car)
+
+    // solve 2: 2. Change the first item of Brand to "Ford".
+    const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+    Brand[0] = "Ford"
+    console.log(Brand)
+    
+    
